a88d9447dc9c82ed287edc3c1149289ca38f7c06
winter limelight <winter_limelight@yahoo.com.au>
2016-12-07 20:19:55 +1300

    First complete xUnit test.

--- AssetManagementApi.UnitTests/Template.fs
+++ AssetManagementApi.UnitTests/Template.fs
@@ -2,13 +2,40 @@
 
 open System
 open Xunit
+open AssetManagementApi
+open AssetManagementApi.DomainInterfaces
+open AssetManagementApi.Commands.Template
+
+type TemplateValidationTestData() =
+        inherit Util.BaseTestData()
+        override this.data = 
+            Seq.ofList [
+                [|
+                    ({ Id = System.Guid.NewGuid(); Name = "abc"; Fields = []; MaintenanceProgramId = System.Guid.Empty } : DomainTypes.Template) :> obj
+                    (Railway.Failure (InvalidTemplate "Template may not have an empty list")) :> Railway.Result<unit,TemplateCommandError> :> obj
+                |] 
+            ]
 
 type TemplateDomainOperationsTests() =
 
-    [<Fact>]
-    let passingTest() =
-        Assert.True(true)
+    let basicTemplate: DomainTypes.Template = { 
+        Id = System.Guid.NewGuid()
+        Name = "templateName"
+        Fields = [{ Id =  System.Guid.NewGuid(); Name = "strFieldName"; Field = DomainTypes.StringField("strFieldValue") }] 
+        MaintenanceProgramId = System.Guid.Empty 
+    }
 
-    [<Fact>]
-    let failingTest() =
-        Assert.True(false)
+    let mockRepo = { new ITemplateWriteRepository with
+        member this.FindById id = Some({basicTemplate with Id = id})
+        member this.Save (template: DomainTypes.Template) = ()
+    }
+
+    [<Theory>]
+    [<ClassData(typeof<TemplateValidationTestData>)>]
+    let ``Template validation`` (input: DomainTypes.Template) (expected: Railway.Result<unit,TemplateCommandError>) =
+        
+        let cmd = TemplateCommand.Create(input)
+        let result : Railway.Result<unit,TemplateCommandError> = TemplateCommandHandler.Execute cmd mockRepo
+        Assert.Equal(result, expected)
+        ()
+
--- AssetManagementApi/Util.fs
+++ AssetManagementApi/Util.fs
@@ -1,4 +1,13 @@
-module Util
+module AssetManagementApi.Util
 open System
+open System.Collections
+open System.Collections.Generic
 
 let inline isNull (x:^T when ^T : not struct) = obj.ReferenceEquals (x, null)
+
+[<AbstractClass>]
+type BaseTestData() =
+    abstract member data: seq<obj[]>
+    interface IEnumerable<obj[]> with 
+        member this.GetEnumerator() : IEnumerator<obj[]> = this.data.GetEnumerator()
+        member this.GetEnumerator() : IEnumerator = this.data.GetEnumerator() :> IEnumerator
